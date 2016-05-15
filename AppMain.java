53343c94b047235e5382f167b0d432375dc3a5df
babx88 <babx88@naver.com>
2016-05-15 12:57:43 +0900

    허프 라인 추출 1차 작업 완료: 직선이 명백한 경우 라인 추출 후 와핑까지 정상완료됨

---
+++ AppMain.java
@@ -0,0 +1,37 @@
+import javafx.animation.Interpolator;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.VPos;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class AppMain extends Application {
+	@Override
+		public void start(Stage stage) throws Exception {
+			Parent root = FXMLLoader.load(getClass().getResource("MainForm.fxml"));
+			Scene scene = new Scene(root);
+			stage.setTitle("Test");
+			stage.setScene(scene);
+			stage.sizeToScene();
+			stage.show();
+			
+		}
+	public static void main(String[] args) {
+		launch(args);
+		
+	}
+}
---
+++ MainForm.java
@@ -0,0 +1,12 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainForm implements javafx.fxml.Initializable {
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+
+}
